f0c7d1e1c73474db6caf8beb8af93670ee07c89e
Alec Patterson <8888alec@gmail.com>
2024-12-15 11:46:53 -0500

    some progress, stopping for kids soccer game

--- Days/Day14/solutino.py
+++ Days/Day14/solutino.py
@@ -20,7 +20,55 @@ def load_robots(path):
                 logging.error(f"No match found for the line: '{line}'")
     return bots
 
+def print_bot_info(robots):
+    for bot in bots:
+        print(bot)
+
+def grid_build(rows, cols):
+    rows = 7
+    cols = 11
+    grid = [[0 for _ in range(cols)] for _ in range(rows)]
+    return grid
+
+def place_bots(grid, bots):
+    for bot in bots:
+        x, y = bot[0]
+        grid[y][x] += 1
+    return grid
+
+def print_grid(grid):
+    for row in grid:
+        print(row)
+
+def simulate_bots(grid, bots):
+    for i in range(100):
+        print(f'After 1 sec: ')
+        for bot in bots:
+            x, y = bot[0]
+            dx, dy = bot[1]
+            if y + dy >= len(grid):
+                new_y = (y + dy) - len(grid) 
+            else:
+                new_y = y + dy
+            if x + dx >= len(grid[0]):
+                new_x = (x + dx) - len(grid[0])
+            else:
+                new_x = x + dx
+
+            grid[y][x] -= 1
+            grid[new_y][new_x] += 1
+            bot[0] = (new_x, new_y)
+        # log bots and grid here
+    return grid, bots
+
 path = r'.\Days\Day14\example.txt'
 bots = load_robots(path)
-for bot in bots:
-    print(bot)
+
+grid = grid_build(7, 11)
+
+grid = place_bots(grid, bots)
+grid, bots = simulate_bots(grid, bots)
+print_grid(grid)
+
+# print_bot_info(bots)
+
